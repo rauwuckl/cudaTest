43924969769bb39cf08797f2f7bf452f28a9fc0e
Ubuntu <ubuntu@ip-172-31-0-201.ec2.internal>
2017-03-12 10:56:50 +0000

    integradet stupid time function

--- matMulCPU.cpp
+++ matMulCPU.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
+#include <chrono>
+#include <functional>
+
+using namespace std::chrono;
+
+typedef std::function<void*() > func;
 
 /**
  * multiply A (an x am ) with B (bn x bm). result will be a pointer to array of an x bm
@@ -78,6 +84,17 @@ void print(int** A, int an, int am){
     }
 }
 
+template <class retType>
+retType measureTime(func& f){
+	std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
+	void* result = f();
+	std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
+
+	auto duration = std::chrono::duration_cast<microseconds>( t2 - t1 ).count();
+	std::cout << "execution took " << duration << " microseconds" << std::endl;
+	return (retType)result;
+}
+
 
 int main(){
 
@@ -93,8 +110,10 @@ int main(){
     int **C = init(300, 500, 1);
     int **D = init(500, 900, 2);
 
-    out=matMul(C, 300, 500, D, 500, 900);
+    func f = [C,D](){return (void*)matMul(C, 300, 500, D, 500, 900);};
+
+    out=measureTime<int**>(f);
     std::cout << "the new one " << std:: endl;
-    print(out, 300, 900);
+//    print(out, 300, 900);
     return 0;
 }
